4d37c161c5df9fd1c8c585d13f3a327b9e5ec8d6
RODRIGO BATISTA FREIRE <21276392@CFP127ALUNO.LOCAL>
2021-09-17 16:06:17 -0300

    Atualização

--- calculoDeImc/src/calculoDeImc/Imc.java
+++ calculoDeImc/src/calculoDeImc/Imc.java
@@ -4,6 +4,12 @@ public class Imc {
 
 	public static void main(String[] args) {
 		
+		double alt = 1.90;
+		double pes = 89;
+		double calc = pes / (alt * alt);
+		
+		System.out.print("O seu I.M.C é: " + calc);
+
 
 	}
 
