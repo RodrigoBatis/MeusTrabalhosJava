cc3cb2f5b037268d878cb357ad050613b7236cd8
RODRIGO BATISTA FREIRE <21276392@CFP127ALUNO.LOCAL>
2021-11-12 16:52:40 -0300

    Novo projeto

---
+++ softwareJogosGui/src/br/senai/sp/jandira/model/Fabricante.java
@@ -0,0 +1,31 @@
+package br.senai.sp.jandira.model;
+
+public class Fabricante {
+		
+		private String nome;
+		private String inauguração;
+		private String localizacao;
+		
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setInauguração(String inauguração) {
+			this.inauguração = inauguração;
+		}
+		public String getInauguração() {
+			return inauguração;
+		}
+		public void setLocalizacao(String localizacao) {
+			this.localizacao = localizacao;
+		}
+		public String getLocalizacao() {
+			return localizacao;
+		}
+		
+		
+
+		
+}
---
+++ softwareJogosGui/src/br/senai/sp/jandira/model/Jogo.java
@@ -0,0 +1,11 @@
+package br.senai.sp.jandira.model;
+
+public class Jogo {
+		
+		private String titulo;
+		private Fabricante fabricante;
+		private Boolean zerado;
+		private String console;
+		private double valor;
+		private String observacao;
+}
